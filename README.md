1903be5b1450ad30771e08039b83fc187b978600
John Matthew <john@compunique.com>
2019-07-31 16:56:50 -0600

    Adjustments for Postgres DB connectivity

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ A basic django app for automation purposes
 
 ## Releases
 
+### 7/31/2019
+- Updte for Postgres values in settings.py
+
 ### 7/30/2019
 - Update for django 2.? in requirements.txt
 - test with ansible-django-stack repo for deployment
--- basicapp/settings.py
+++ basicapp/settings.py
@@ -73,12 +73,26 @@ WSGI_APPLICATION = 'basicapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+# check for Postgres stuff
+if "DATABASE_USER" in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('DATABASE_NAME'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
